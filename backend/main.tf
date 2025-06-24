9fe90e3cd61f56ede5a468c535e139f486f65d16
mikhael villamor <mikhaelvillamor97@gmail.com>
2025-06-23 22:55:52 -0700

    testing backened cicd

--- backend/main.tf
+++ backend/main.tf
@@ -33,6 +33,18 @@ resource "aws_dynamodb_table" "dev_table" {
   }
 }
 
+resource "aws_dynamodb_table" "dev_table_test" {
+  name = "website-visitors-test"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+
 resource "aws_dynamodb_table_item" "dev_item" {
   table_name = aws_dynamodb_table.dev_table.name
   hash_key   = "id"
