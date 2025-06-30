77e2942d7d00ebab5d4edcd685e25764e9929787
mikhael villamor <mikhaelvillamor97@gmail.com>
2025-06-29 23:03:06 -0700

    removed test bucket

--- frontend/main.tf
+++ frontend/main.tf
@@ -33,10 +33,6 @@ resource "aws_s3_bucket" "dev_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket" "dev_bucket1" {
-  bucket = "test5384509238452394"
-  force_destroy = true
-}
 resource "aws_s3_bucket_server_side_encryption_configuration" "dev_bucket_encryption" {
   bucket = aws_s3_bucket.dev_bucket.id
 
