cd46ce631c3cfde46d56c8cf981ad6359f5f6d6e
mikhael villamor <mikhaelvillamor97@gmail.com>
2025-07-22 22:33:09 -0700

    added version and requirements

--- backend/main.tf
+++ backend/main.tf
@@ -3,7 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.81.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.0"
     }
   }
   backend "s3" {
